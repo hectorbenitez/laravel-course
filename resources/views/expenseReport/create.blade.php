5ea225eea193bfe663119356af2f0c90a48e753d
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-04 19:35:13 -0600

    Creating form to add a new report

--- resources/views/expenseReport/create.blade.php
+++ resources/views/expenseReport/create.blade.php
@@ -13,7 +13,13 @@
     </div>
     <row>
         <div class="col">
-            Form
+            <form action="/expense_reports" method="POST">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     </row>
 @endsection
