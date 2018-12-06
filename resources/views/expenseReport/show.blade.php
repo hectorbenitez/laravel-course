9c2a64618747fcbc291b68edc2773a3156d00834
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-05 22:29:57 -0600

    show expenses in report

--- resources/views/expenseReport/show.blade.php
+++ resources/views/expenseReport/show.blade.php
@@ -12,6 +12,22 @@
         </div>
     </div>
     <row>
-
+        <div class="col">
+            <h2>Expenses</h2>
+            <table class="table">
+                <tr>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Date</th>
+                </tr>
+                @foreach($report->expenses as $expense)
+                    <tr>
+                        <td>{{ $expense->description }}</td>
+                        <td>{{ $expense->amount }}</td>
+                        <td>{{ $expense->created_at }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </row>
 @endsection
