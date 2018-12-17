9d49ef5d76f9533592d5d073fef1cd2cb1d6b4f4
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-17 14:51:36 -0500

    expense report form

---
+++ resources/views/expenseReport/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1>New Report</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-secondary" href="/expense_reports">Back</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <form action="/expense_reports" method="POST">
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
+                </div>
+                <button class="btn btn-primary" type="submit" >Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -4,6 +4,15 @@
     <div class="row">
         <div class="col">
             <h1>Reports</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="/expense_reports/create">Create a new report</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
             <table class="table">
                 @foreach($expenseReports as $expenseReport)
                     <tr>
