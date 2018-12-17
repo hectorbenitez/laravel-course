54cab95758add1e78a501d5ff4fec1de52721722
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-17 10:07:13 -0500

    request

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         return view('test', [
-            'title' => 'Curso Laravel en Platzi!!!!!'
+            'title' => $request->query('title', 'Valor default')
         ]);
     }
 }
