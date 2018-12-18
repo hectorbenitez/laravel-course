54d36be9fa891e7f36c80094b7ce4e07c8406bf5
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-18 08:32:01 -0500

    csrf

--- app/Http/Controllers/ExpenseReportController.php
+++ app/Http/Controllers/ExpenseReportController.php
@@ -37,7 +37,11 @@ class ExpenseReportController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $report = new ExpenseReport();
+        $report->title = $request->get('title');
+        $report->save();
+
+        return redirect('/expense_reports');
     }
 
     /**
--- resources/views/expenseReport/create.blade.php
+++ resources/views/expenseReport/create.blade.php
@@ -14,6 +14,7 @@
     <div class="row">
         <div class="col">
             <form action="/expense_reports" method="POST">
+                @csrf
                 <div class="form-group">
                     <label for="title">Title:</label>
                     <input type="text" class="form-control" id="title" name="title" placeholder="Type a title">
